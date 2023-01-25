14332f3865b7ca1d70cd481e15cde839de5bba7c
Jovan <94451344+Wolfcode16@users.noreply.github.com>
2023-01-25 16:25:37 +0100

    Delete Lab-files directory

--- Lab-files/manhat.m
+++
@@ -1,9 +0,0 @@
-function [Distance] = manhat(x,y,u,v)
-% Manhattan avståndet mellan två punkter i planet
-% Första punkten: (x,y)
-% Andra punkten: (u,v)
-
-Distance = (abs(x-u) + abs(y-v));
-
-end
-
